0784e6766cadde307806bf05e0179db51c24657c
Indika Senarathna <indika.s@hsuite.lk>
2024-11-26 08:16:27 +0530

    updated

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,10 @@
 public class Test {
+
+    public static void main(String[] args) {
+        String[] a = {"jdskfdhjhfdj"}; // Array with one element
+
+        for (int i = 0; i < a.length; i++) { // Corrected loop condition
+            System.out.println(a[]); // Access and print each element
+        }
+    }
 }
